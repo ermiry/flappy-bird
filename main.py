cddaec5485fbf5f55a8f4f8dce48115392cb4422
ermiry <erick.salas@ermiry.com>
2021-02-23 23:08:04 -0600

    Refactored main loop to handle pipes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pygame
 
 from bird import Bird
+from pipe import Pipe
 
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
@@ -9,20 +10,25 @@ WIN_HEIGHT = 800
 WIN = pygame.display.set_mode ((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption ("Flappy Bird")
 
-pipe_img = pygame.transform.scale2x (pygame.image.load (os.path.join ("assets", "pipe.png")).convert_alpha ())
 bg_img = pygame.transform.scale (pygame.image.load (os.path.join ("assets", "bg.png")).convert_alpha (), (600, 900))
-base_img = pygame.transform.scale2x (pygame.image.load (os.path.join ("assets", "base.png")).convert_alpha ())
 
-def draw(win, bird):
+def draw(win, bird, pipes, base):
 	win.blit (bg_img, (0, 0))
+
+	for p in pipes:
+		p.draw (win)
+
+	base.draw (win)
+
 	bird.draw (win)
+
 	pygame.display.update ()
 
 def main():
 	clock = pygame.time.Clock ()
 	win = pygame.display.set_mode ((WIN_WIDTH, WIN_HEIGHT))
 
-	bird = Bird (200, 200)
+	bird = Bird (230, 350)
 
 	run = True
 	while run:
